0f6862c9af163c2677b716b3bb6e897616d03adc
sudhagar-g <sudhagarg449@gmail.com>
2025-09-19 14:28:47 +0530

    .

--- form.py
+++ form.py
@@ -1,5 +1,20 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField
+from wtforms import StringField, PasswordField , SubmitField, BooleanField
+from wtforms.validators import DataRequired, Length, Email, EqualTo
 
-class RegistrationFrom(FlaskForm):
-    username = StringField('username')
+
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Username',validators=[DataRequired(),Length(min=2, max=20)])
+    email = StringField('Email',validators=[DataRequired(),Email()])
+    password = PasswordField('Password',validators=[DataRequired()])
+    confirm = PasswordField('Confirm password',validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('sign up')
+
+
+class LoginForm(FlaskForm):
+    email = StringField('Email',validators=[DataRequired(),Email()])
+    password = PasswordField('Password',validators=[DataRequired()])
+    remember = BooleanField('Remember Me')
+    submit = SubmitField('Login')    
+
--- webPage.py
+++ webPage.py
@@ -1,13 +1,15 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template,flash, url_for, redirect
+from form import RegistrationForm,LoginForm
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = '2884384d35cf50674db944425e67c98c'
 
 data = [
     {
         "title":"flask ",
         "author":"Tom",
         "content":"Today i start learn about flask",
-        "date_posted":"sep-16-2025 - 5:0pm"
+        "date_posted":"sep-16-2025 - 5:00pm"
     },
     {
         "title":"Again flask",
@@ -28,5 +30,19 @@ def hello():
 def about():
     return render_template("about.html",title="about")
 
+
+@app.route("/register" ,methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!','success')
+        return redirect (url_for('hello'))
+    return render_template('register.html',title ='Register', form=form)
+
+@app.route("/login")
+def login():
+    form = LoginForm()
+    return render_template("login.html",title="Login",form=form)
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True) 

6b8fc8fdf3d2a8f32757bf9500e0bde7be9a5b63
sudhagar-g <sudhagarg449@gmail.com>
2025-09-22 03:13:23 +0530

    .

--- webPage.py
+++ webPage.py
@@ -1,8 +1,41 @@
+
+from datetime import datetime
 from flask import Flask, render_template,flash, url_for, redirect
+from flask_sqlalchemy import SQLAlchemy
 from form import RegistrationForm,LoginForm
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '2884384d35cf50674db944425e67c98c'
+
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:\\\site.db'
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer,primary_key = True)
+    username = db.Column(db.String(20),unique = True, nullable = False)
+    email = db.Column(db.String(120),unique = True, nullable = False)
+    image_file = db.Column(db.String, nullable = False, default = 'ddd.jpg')
+    password = db.Column(db.String(60),nullable =False )
+    posts = db.relationship('Post', backref='author', lazy = True)
+
+
+    def __repr__(self):
+        return f"User('{self.username}','{self.email}','{self.image_file}')"
+
+
+class Post(db.Model):
+    id = db.Column(db.Integer,primary_key = True)
+    title = db.Column(db.String(100),nullable = False)
+    date_post = db.Column(db.DateTime, nullable=False, default =datetime.utcnow)
+    content = db.Column(db.Text,nullable = False)
+    user_id = db.Column(db.Integer,db.ForeignKey('user.id'),nullable=False)
+
+    def __repr__(self):
+        return f"Post('{self.title}','{self.date_post}')"
+
+
+
 
 data = [
     {
@@ -39,10 +72,19 @@ def register():
         return redirect (url_for('hello'))
     return render_template('register.html',title ='Register', form=form)
 
-@app.route("/login")
+@app.route("/login",methods=['GET', 'POST'])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
+            flash('you have been logged in!','success')
+            return redirect(url_for('hello'))
+        else:
+            flash('login unsucessful.please check username and password','danger')
     return render_template("login.html",title="Login",form=form)
 
+
+
+
 if __name__ == "__main__":
-    app.run(debug=True) 
+   app.run(debug=True) 

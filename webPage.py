41e4fe1293593d3c0e092be8f3cc955be91f4562
sudhagar-g <sudhagarg449@gmail.com>
2025-09-25 08:47:22 +0530

    .

--- webPage.py
+++ webPage.py
@@ -1,4 +1,4 @@
-
+import os
 from datetime import datetime
 from flask import Flask, render_template,flash, url_for, redirect
 from flask_sqlalchemy import SQLAlchemy
@@ -7,15 +7,15 @@ from form import RegistrationForm,LoginForm
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '2884384d35cf50674db944425e67c98c'
 
-
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:\\\site.db'
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI'] = f"sqlite:///{os.path.join(basedir,'site.db')}"
 db = SQLAlchemy(app)
 
 class User(db.Model):
     id = db.Column(db.Integer,primary_key = True)
     username = db.Column(db.String(20),unique = True, nullable = False)
     email = db.Column(db.String(120),unique = True, nullable = False)
-    image_file = db.Column(db.String, nullable = False, default = 'ddd.jpg')
+    image_file = db.Column(db.String(20), nullable = False, default = 'ddd.jpg')
     password = db.Column(db.String(60),nullable =False )
     posts = db.relationship('Post', backref='author', lazy = True)
 
@@ -87,4 +87,6 @@ def login():
 
 
 if __name__ == "__main__":
+   with app.app_context():
+       db.create_all()
    app.run(debug=True) 
